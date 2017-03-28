46dd6c8e60b36396d1078ef618f74bb30599ef97
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-03-27 19:13:21 -0500

    Creates BaseViewModel to reduce redundancy in ViewModels.

--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -5,9 +5,9 @@ using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
-    public class SearchJobsViewModel 
+    public class SearchJobsViewModel : BaseViewModel
     {
-        // TODO #7.1 - Extract members common to JobFieldsViewModel
+        // DONE #7.1 - Extract members common to JobFieldsViewModel
         // to BaseViewModel
 
         // The search results
@@ -20,24 +20,8 @@ namespace TechJobs.ViewModels
         [Display(Name = "Keyword:")]
         public string Value { get; set; } = "";
 
-        // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
-
-        // View title
-        public string Title { get; set; } = "";
-
-        public SearchJobsViewModel()
+        public SearchJobsViewModel() : base()
         {
-            // Populate the list of all columns
-
-            Columns = new List<JobFieldType>();
-
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-            {
-                Columns.Add(enumVal);
-            }
-
-
         }
     }
 }
